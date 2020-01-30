3d14ad76696d74e2aeb2f26e7f0ab9abc9e4c3f1
miftahfaridh <muhammadmiftahfaridh@gmail.com>
2020-01-30 16:44:18 +0700

    WOI

--- scripts/simple_out.py
+++ scripts/simple_out.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
 import time
-import rospy
+# import rospy
 
 # Pin Definitions
-output_pin = 18  # BOARD pin 12, BCM pin 18
+input_pin = 18  # BOARD pin 12, BCM pin 18
 
 def main():
     # Pin Setup:
     # Board pin-numbering scheme
     GPIO.setmode(GPIO.BCM)
     # set pin as an output pin with optional initial state of HIGH
-    GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(input_pin, GPIO.IN,  pull_up_down=GPIO.PUD_DOWN)
 
     curr_value = GPIO.HIGH
-    while not rospy.is_shutdown():
+    while True:
         # Toggle the output every second
-        GPIO.output(output_pin, curr_value)
-
-    GPIO.output(output_pin, GPIO.LOW)
+        a = GPIO.input(input_pin)
+        print(a)
+    # GPIO.output(output_pin, GPIO.LOW)
     GPIO.cleanup()
 
 if __name__ == '__main__':
-    rospy.init_node("led")
+    # rospy.init_node("led")
     main()
