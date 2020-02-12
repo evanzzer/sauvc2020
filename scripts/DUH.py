3bdfab02656ee521fd66c933f6b007d4ede05876
miftahfaridh <muhammadmiftahfaridh@gmail.com>
2020-02-12 15:39:17 +0700

    ngubah

---
+++ scripts/DUH.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+
+import os
+import numpy as np
+import cv2
+from skimage import exposure
+from cv_bridge import CvBridge, CvBridgeError
+import rospy
+from sensor_msgs.msg import Image
+
+def nothing(angka):
+    pass
+
+def RecoverCLAHE(sceneRadiance):
+    clahe = cv2.createCLAHE(clipLimit=2, tileGridSize=(4, 4))
+    for i in range(3):
+
+        # sceneRadiance[:, :, i] =  cv2.equalizeHist(sceneRadiance[:, :, i])
+        sceneRadiance[:, :, i] = clahe.apply((sceneRadiance[:, :, i]))
+
+
+    return sceneRadiance
+
+rospy.init_node("MAINA")
+image_publisher = rospy.Publisher("/auv/image", Image, queue_size=8)
+
+np.seterr(over='ignore')
+# cv2.startWindowThread()
+cv2.namedWindow("Trackbar")
+# cv2.namedWindow("apa")
+cv2.namedWindow("op")
+cv2.namedWindow("clahe")
+cv2.createTrackbar("x", "Trackbar" , 0, 1024, nothing)
+cv2.createTrackbar("y", "Trackbar" , 0, 768, nothing)
+TH = 40
+vid = cv2.VideoCapture(0)
+bridge = CvBridge()
+
+while not rospy.is_shutdown():
+    x = cv2.getTrackbarPos("x", "Trackbar")
+    y = cv2.getTrackbarPos("y", "Trackbar")
+    # img = cv2.imread('gambar.png')
+    success, img = vid.read()
+    if img is None:
+        break
+    new_img = img.copy()
+    new_img = RecoverCLAHE(new_img)
+
+    imgmsg = bridge.cv2_to_imgmsg(new_img, "bgr8")
+    image_publisher.publish(imgmsg)
+
+    # cv2.imshow('clahe', new_img)
+    # cv2.imshow('apa', img)
+    roi = new_img[y-5:y+5, x-5:x+5]
+    hue = np.mean(roi[:,:, 0])
+    sat = np.mean(roi[:,:, 1])
+    val = np.mean(roi[:,:, 2])
+    h_low = hue - TH
+    h_high = hue + TH
+    s_low = sat - TH
+    s_high = sat + TH
+    v_low = val - TH
+    v_high = val + TH
+    print(h_low, h_high, s_high, s_low, v_high,v_low)
+    # h_low = 74
+    # s_low = 84
+    # v_low = 100
+    # h_high = 164
+    # s_high = 174
+    # v_high = 190
+    # print("low ", end='')
+    # print((h_low, s_low, v_low))
+    # print("high ", end='')
+    # print((h_high, s_high, v_high))
+    cv2.circle(new_img, (x,y), 5, (255, 255, 255), thickness=1, lineType=8, shift=0)
+    mask1 = cv2.inRange(new_img, (h_low, s_low, v_low), (h_high, s_high, v_high))
+    kernel = np.ones((15, 2) ,np.uint8)
+    erosion = cv2.erode(mask1,kernel,iterations = 1)
+    (_,contours,hierarchy)= cv2.findContours(erosion, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area < 200:
+            continue
+        x,y,w,h = cv2.boundingRect(cnt)
+        # cv2.rectangle(new_img, (x, y), (x + w, y + h), (0, 255,0), 2)
+    # cv2.imshow("apa", img)
+    cv2.imshow("clahe", new_img)
+    cv2.imshow("op", erosion)
+    cv2.waitKey(30)
